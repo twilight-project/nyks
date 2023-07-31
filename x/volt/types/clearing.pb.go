ed8b8f2480879dea7978b39b0f798a02c0ac5533
Hammad Tariq <hammadtariq@yahoo.co.uk>
2023-07-31 16:38:22 +0400

    removed QQAccount from ClearingAccount proto as no longer needed

--- x/volt/types/clearing.pb.go
+++ x/volt/types/clearing.pb.go
@@ -76,80 +76,19 @@ func (m *IndividualTwilightReserveAccountBalance) GetAmount() uint64 {
 	return 0
 }
 
-type QQAccount struct {
-	Nonce           uint64 `protobuf:"varint,1,opt,name=nonce,proto3" json:"nonce,omitempty"`
-	DepositAccount  string `protobuf:"bytes,2,opt,name=depositAccount,proto3" json:"depositAccount,omitempty"`
-	WithdrawAccount string `protobuf:"bytes,3,opt,name=withdrawAccount,proto3" json:"withdrawAccount,omitempty"`
-}
-
-func (m *QQAccount) Reset()         { *m = QQAccount{} }
-func (m *QQAccount) String() string { return proto.CompactTextString(m) }
-func (*QQAccount) ProtoMessage()    {}
-func (*QQAccount) Descriptor() ([]byte, []int) {
-	return fileDescriptor_1c47080687c58e5f, []int{1}
-}
-func (m *QQAccount) XXX_Unmarshal(b []byte) error {
-	return m.Unmarshal(b)
-}
-func (m *QQAccount) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	if deterministic {
-		return xxx_messageInfo_QQAccount.Marshal(b, m, deterministic)
-	} else {
-		b = b[:cap(b)]
-		n, err := m.MarshalToSizedBuffer(b)
-		if err != nil {
-			return nil, err
-		}
-		return b[:n], nil
-	}
-}
-func (m *QQAccount) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_QQAccount.Merge(m, src)
-}
-func (m *QQAccount) XXX_Size() int {
-	return m.Size()
-}
-func (m *QQAccount) XXX_DiscardUnknown() {
-	xxx_messageInfo_QQAccount.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_QQAccount proto.InternalMessageInfo
-
-func (m *QQAccount) GetNonce() uint64 {
-	if m != nil {
-		return m.Nonce
-	}
-	return 0
-}
-
-func (m *QQAccount) GetDepositAccount() string {
-	if m != nil {
-		return m.DepositAccount
-	}
-	return ""
-}
-
-func (m *QQAccount) GetWithdrawAccount() string {
-	if m != nil {
-		return m.WithdrawAccount
-	}
-	return ""
-}
-
 // ClearingAccount is used to keep a mapping of how a user's addresses and its reserve account balances
 type ClearingAccount struct {
 	TwilightAddress        string                                     `protobuf:"bytes,1,opt,name=TwilightAddress,proto3" json:"TwilightAddress,omitempty"`
 	BtcDepositAddress      string                                     `protobuf:"bytes,2,opt,name=BtcDepositAddress,proto3" json:"BtcDepositAddress,omitempty"`
 	BtcWithdrawAddress     string                                     `protobuf:"bytes,3,opt,name=BtcWithdrawAddress,proto3" json:"BtcWithdrawAddress,omitempty"`
-	QQAccounts             []*QQAccount                               `protobuf:"bytes,4,rep,name=QQAccounts,proto3" json:"QQAccounts,omitempty"`
-	ReserveAccountBalances []*IndividualTwilightReserveAccountBalance `protobuf:"bytes,5,rep,name=ReserveAccountBalances,proto3" json:"ReserveAccountBalances,omitempty"`
+	ReserveAccountBalances []*IndividualTwilightReserveAccountBalance `protobuf:"bytes,4,rep,name=ReserveAccountBalances,proto3" json:"ReserveAccountBalances,omitempty"`
 }
 
 func (m *ClearingAccount) Reset()         { *m = ClearingAccount{} }
 func (m *ClearingAccount) String() string { return proto.CompactTextString(m) }
 func (*ClearingAccount) ProtoMessage()    {}
 func (*ClearingAccount) Descriptor() ([]byte, []int) {
-	return fileDescriptor_1c47080687c58e5f, []int{2}
+	return fileDescriptor_1c47080687c58e5f, []int{1}
 }
 func (m *ClearingAccount) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -199,13 +138,6 @@ func (m *ClearingAccount) GetBtcWithdrawAddress() string {
 	return ""
 }
 
-func (m *ClearingAccount) GetQQAccounts() []*QQAccount {
-	if m != nil {
-		return m.QQAccounts
-	}
-	return nil
-}
-
 func (m *ClearingAccount) GetReserveAccountBalances() []*IndividualTwilightReserveAccountBalance {
 	if m != nil {
 		return m.ReserveAccountBalances
@@ -215,37 +147,32 @@ func (m *ClearingAccount) GetReserveAccountBalances() []*IndividualTwilightReser
 
 func init() {
 	proto.RegisterType((*IndividualTwilightReserveAccountBalance)(nil), "twilightproject.nyks.volt.IndividualTwilightReserveAccountBalance")
-	proto.RegisterType((*QQAccount)(nil), "twilightproject.nyks.volt.QQAccount")
 	proto.RegisterType((*ClearingAccount)(nil), "twilightproject.nyks.volt.ClearingAccount")
 }
 
 func init() { proto.RegisterFile("nyks/volt/clearing.proto", fileDescriptor_1c47080687c58e5f) }
 
 var fileDescriptor_1c47080687c58e5f = []byte{
-	// 362 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x92, 0xdf, 0x6a, 0xea, 0x40,
-	0x10, 0xc6, 0x8d, 0xff, 0x20, 0x73, 0xe0, 0xc8, 0x59, 0x0e, 0x92, 0x42, 0x09, 0x22, 0xa5, 0xf5,
-	0xa2, 0x26, 0xd0, 0x3e, 0x81, 0xa9, 0x50, 0xbc, 0x34, 0x14, 0x0a, 0xbd, 0x29, 0x71, 0x77, 0xd1,
-	0x6d, 0xe3, 0x6e, 0xc8, 0xae, 0x5a, 0xfb, 0x14, 0x7d, 0xac, 0x5e, 0x7a, 0x59, 0xe8, 0x4d, 0xd1,
-	0x17, 0x29, 0x6e, 0x36, 0x56, 0xac, 0x96, 0x5e, 0xee, 0xcc, 0x6f, 0xbe, 0xf9, 0x76, 0x66, 0xc0,
-	0xe1, 0xf3, 0x47, 0xe9, 0x4f, 0x45, 0xac, 0x7c, 0x1c, 0xd3, 0x28, 0x65, 0x7c, 0xe8, 0x25, 0xa9,
-	0x50, 0x02, 0x1d, 0xa9, 0x19, 0x8b, 0xd9, 0x70, 0xa4, 0x92, 0x54, 0x3c, 0x50, 0xac, 0xbc, 0x35,
-	0xe9, 0xad, 0xc9, 0xe6, 0x3d, 0x9c, 0xf5, 0x38, 0x61, 0x53, 0x46, 0x26, 0x51, 0x7c, 0x63, 0xb0,
-	0x90, 0x4a, 0x9a, 0x4e, 0x69, 0x07, 0x63, 0x31, 0xe1, 0x2a, 0x88, 0xe2, 0x88, 0x63, 0x8a, 0x8e,
-	0xc1, 0x36, 0x89, 0x1e, 0x71, 0xac, 0x86, 0xd5, 0x2a, 0x87, 0x5f, 0x01, 0x54, 0x87, 0x6a, 0x67,
-	0xbc, 0xc6, 0x9d, 0xa2, 0x4e, 0x99, 0x57, 0x53, 0x82, 0xdd, 0xef, 0x1b, 0x25, 0xf4, 0x1f, 0x2a,
-	0x5c, 0x70, 0x4c, 0x4d, 0x79, 0xf6, 0x40, 0xa7, 0xf0, 0x97, 0xd0, 0x44, 0x48, 0xa6, 0x0c, 0xa7,
-	0x25, 0xec, 0x70, 0x27, 0x8a, 0x5a, 0x50, 0x9b, 0x31, 0x35, 0x22, 0x69, 0x34, 0xcb, 0xc1, 0x92,
-	0x06, 0x77, 0xc3, 0xcd, 0xf7, 0x22, 0xd4, 0xae, 0xcc, 0x0c, 0xb6, 0xaa, 0xf3, 0xff, 0x75, 0x08,
-	0x49, 0xa9, 0x94, 0xda, 0x85, 0x1d, 0xee, 0x86, 0xd1, 0x39, 0xfc, 0x0b, 0x14, 0xee, 0x9a, 0xe6,
-	0x86, 0xcd, 0x2c, 0x7d, 0x4f, 0x20, 0x0f, 0x50, 0xa0, 0xf0, 0x6d, 0xee, 0xc0, 0xe0, 0x99, 0xb1,
-	0x3d, 0x19, 0xd4, 0x05, 0xd8, 0x0c, 0x44, 0x3a, 0xe5, 0x46, 0xa9, 0xf5, 0xe7, 0xe2, 0xc4, 0x3b,
-	0xb8, 0x21, 0x6f, 0x03, 0x87, 0x5b, 0x75, 0xe8, 0x19, 0xea, 0x7b, 0xb7, 0x24, 0x9d, 0x8a, 0x56,
-	0x0c, 0x7e, 0x50, 0xfc, 0xe5, 0xc2, 0xc3, 0x03, 0x1d, 0x82, 0xeb, 0xd7, 0xa5, 0x6b, 0x2d, 0x96,
-	0xae, 0xf5, 0xb1, 0x74, 0xad, 0x97, 0x95, 0x5b, 0x58, 0xac, 0xdc, 0xc2, 0xdb, 0xca, 0x2d, 0xdc,
-	0xb5, 0x87, 0x4c, 0x8d, 0x26, 0x03, 0x0f, 0x8b, 0xb1, 0x9f, 0xf7, 0x6f, 0x1b, 0x03, 0xbe, 0x3e,
-	0xcf, 0xa7, 0xec, 0x40, 0xd5, 0x3c, 0xa1, 0x72, 0x50, 0xd5, 0xe7, 0x79, 0xf9, 0x19, 0x00, 0x00,
-	0xff, 0xff, 0xa6, 0x8f, 0xa3, 0x78, 0xba, 0x02, 0x00, 0x00,
+	// 303 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0xc8, 0xab, 0xcc, 0x2e,
+	0xd6, 0x2f, 0xcb, 0xcf, 0x29, 0xd1, 0x4f, 0xce, 0x49, 0x4d, 0x2c, 0xca, 0xcc, 0x4b, 0xd7, 0x2b,
+	0x28, 0xca, 0x2f, 0xc9, 0x17, 0x92, 0x2c, 0x29, 0xcf, 0xcc, 0xc9, 0x4c, 0xcf, 0x28, 0x29, 0x28,
+	0xca, 0xcf, 0x4a, 0x4d, 0x2e, 0xd1, 0x03, 0xa9, 0xd4, 0x03, 0xa9, 0x54, 0x8a, 0xe7, 0x52, 0xf7,
+	0xcc, 0x4b, 0xc9, 0x2c, 0xcb, 0x4c, 0x29, 0x4d, 0xcc, 0x09, 0x81, 0x2a, 0x0b, 0x4a, 0x2d, 0x4e,
+	0x2d, 0x2a, 0x4b, 0x75, 0x4c, 0x4e, 0xce, 0x2f, 0xcd, 0x2b, 0x71, 0x4a, 0xcc, 0x49, 0xcc, 0x4b,
+	0x4e, 0x15, 0x92, 0xe1, 0xe2, 0x84, 0x4a, 0x78, 0xa6, 0x48, 0x30, 0x2a, 0x30, 0x6a, 0xb0, 0x04,
+	0x21, 0x04, 0x84, 0xc4, 0xb8, 0xd8, 0x1c, 0x73, 0x41, 0xca, 0x25, 0x98, 0xc0, 0x52, 0x50, 0x9e,
+	0xd2, 0x54, 0x26, 0x2e, 0x7e, 0x67, 0xa8, 0x73, 0xa0, 0x06, 0x0a, 0x69, 0x70, 0xf1, 0xc3, 0xac,
+	0x72, 0x4c, 0x49, 0x29, 0x4a, 0x2d, 0x2e, 0x06, 0x9b, 0xc7, 0x19, 0x84, 0x2e, 0x2c, 0xa4, 0xc3,
+	0x25, 0xe8, 0x54, 0x92, 0xec, 0x92, 0x5a, 0x90, 0x5f, 0x9c, 0x09, 0x57, 0xcb, 0x04, 0x56, 0x8b,
+	0x29, 0x21, 0xa4, 0xc7, 0x25, 0xe4, 0x54, 0x92, 0x1c, 0x9e, 0x59, 0x92, 0x91, 0x52, 0x94, 0x58,
+	0x0e, 0x53, 0xce, 0x0c, 0x56, 0x8e, 0x45, 0x46, 0xa8, 0x8a, 0x4b, 0x0c, 0xab, 0x57, 0x8b, 0x25,
+	0x58, 0x14, 0x98, 0x35, 0xb8, 0x8d, 0x9c, 0xf4, 0x70, 0x06, 0x9c, 0x1e, 0x91, 0xa1, 0x16, 0x84,
+	0xc3, 0x06, 0x27, 0xf7, 0x13, 0x8f, 0xe4, 0x18, 0x2f, 0x3c, 0x92, 0x63, 0x7c, 0xf0, 0x48, 0x8e,
+	0x71, 0xc2, 0x63, 0x39, 0x86, 0x0b, 0x8f, 0xe5, 0x18, 0x6e, 0x3c, 0x96, 0x63, 0x88, 0xd2, 0x4d,
+	0xcf, 0x2c, 0xc9, 0x28, 0x4d, 0xd2, 0x4b, 0xce, 0xcf, 0xd5, 0x87, 0xd9, 0xaf, 0x0b, 0x75, 0x80,
+	0x3e, 0x38, 0x8e, 0x2b, 0x20, 0xb1, 0x5c, 0x52, 0x59, 0x90, 0x5a, 0x9c, 0xc4, 0x06, 0x8e, 0x63,
+	0x63, 0x40, 0x00, 0x00, 0x00, 0xff, 0xff, 0xcd, 0x6d, 0x4f, 0x5f, 0xff, 0x01, 0x00, 0x00,
 }
 
 func (m *IndividualTwilightReserveAccountBalance) Marshal() (dAtA []byte, err error) {
@@ -281,48 +208,6 @@ func (m *IndividualTwilightReserveAccountBalance) MarshalToSizedBuffer(dAtA []by
 	return len(dAtA) - i, nil
 }
 
-func (m *QQAccount) Marshal() (dAtA []byte, err error) {
-	size := m.Size()
-	dAtA = make([]byte, size)
-	n, err := m.MarshalToSizedBuffer(dAtA[:size])
-	if err != nil {
-		return nil, err
-	}
-	return dAtA[:n], nil
-}
-
-func (m *QQAccount) MarshalTo(dAtA []byte) (int, error) {
-	size := m.Size()
-	return m.MarshalToSizedBuffer(dAtA[:size])
-}
-
-func (m *QQAccount) MarshalToSizedBuffer(dAtA []byte) (int, error) {
-	i := len(dAtA)
-	_ = i
-	var l int
-	_ = l
-	if len(m.WithdrawAccount) > 0 {
-		i -= len(m.WithdrawAccount)
-		copy(dAtA[i:], m.WithdrawAccount)
-		i = encodeVarintClearing(dAtA, i, uint64(len(m.WithdrawAccount)))
-		i--
-		dAtA[i] = 0x1a
-	}
-	if len(m.DepositAccount) > 0 {
-		i -= len(m.DepositAccount)
-		copy(dAtA[i:], m.DepositAccount)
-		i = encodeVarintClearing(dAtA, i, uint64(len(m.DepositAccount)))
-		i--
-		dAtA[i] = 0x12
-	}
-	if m.Nonce != 0 {
-		i = encodeVarintClearing(dAtA, i, uint64(m.Nonce))
-		i--
-		dAtA[i] = 0x8
-	}
-	return len(dAtA) - i, nil
-}
-
 func (m *ClearingAccount) Marshal() (dAtA []byte, err error) {
 	size := m.Size()
 	dAtA = make([]byte, size)
@@ -347,20 +232,6 @@ func (m *ClearingAccount) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		for iNdEx := len(m.ReserveAccountBalances) - 1; iNdEx >= 0; iNdEx-- {
 			{
 				size, err := m.ReserveAccountBalances[iNdEx].MarshalToSizedBuffer(dAtA[:i])
-				if err != nil {
-					return 0, err
-				}
-				i -= size
-				i = encodeVarintClearing(dAtA, i, uint64(size))
-			}
-			i--
-			dAtA[i] = 0x2a
-		}
-	}
-	if len(m.QQAccounts) > 0 {
-		for iNdEx := len(m.QQAccounts) - 1; iNdEx >= 0; iNdEx-- {
-			{
-				size, err := m.QQAccounts[iNdEx].MarshalToSizedBuffer(dAtA[:i])
 				if err != nil {
 					return 0, err
 				}
@@ -421,26 +292,6 @@ func (m *IndividualTwilightReserveAccountBalance) Size() (n int) {
 	return n
 }
 
-func (m *QQAccount) Size() (n int) {
-	if m == nil {
-		return 0
-	}
-	var l int
-	_ = l
-	if m.Nonce != 0 {
-		n += 1 + sovClearing(uint64(m.Nonce))
-	}
-	l = len(m.DepositAccount)
-	if l > 0 {
-		n += 1 + l + sovClearing(uint64(l))
-	}
-	l = len(m.WithdrawAccount)
-	if l > 0 {
-		n += 1 + l + sovClearing(uint64(l))
-	}
-	return n
-}
-
 func (m *ClearingAccount) Size() (n int) {
 	if m == nil {
 		return 0
@@ -458,12 +309,6 @@ func (m *ClearingAccount) Size() (n int) {
 	l = len(m.BtcWithdrawAddress)
 	if l > 0 {
 		n += 1 + l + sovClearing(uint64(l))
-	}
-	if len(m.QQAccounts) > 0 {
-		for _, e := range m.QQAccounts {
-			l = e.Size()
-			n += 1 + l + sovClearing(uint64(l))
-		}
 	}
 	if len(m.ReserveAccountBalances) > 0 {
 		for _, e := range m.ReserveAccountBalances {
@@ -547,139 +392,6 @@ func (m *IndividualTwilightReserveAccountBalance) Unmarshal(dAtA []byte) error {
 					break
 				}
 			}
-		default:
-			iNdEx = preIndex
-			skippy, err := skipClearing(dAtA[iNdEx:])
-			if err != nil {
-				return err
-			}
-			if (skippy < 0) || (iNdEx+skippy) < 0 {
-				return ErrInvalidLengthClearing
-			}
-			if (iNdEx + skippy) > l {
-				return io.ErrUnexpectedEOF
-			}
-			iNdEx += skippy
-		}
-	}
-
-	if iNdEx > l {
-		return io.ErrUnexpectedEOF
-	}
-	return nil
-}
-func (m *QQAccount) Unmarshal(dAtA []byte) error {
-	l := len(dAtA)
-	iNdEx := 0
-	for iNdEx < l {
-		preIndex := iNdEx
-		var wire uint64
-		for shift := uint(0); ; shift += 7 {
-			if shift >= 64 {
-				return ErrIntOverflowClearing
-			}
-			if iNdEx >= l {
-				return io.ErrUnexpectedEOF
-			}
-			b := dAtA[iNdEx]
-			iNdEx++
-			wire |= uint64(b&0x7F) << shift
-			if b < 0x80 {
-				break
-			}
-		}
-		fieldNum := int32(wire >> 3)
-		wireType := int(wire & 0x7)
-		if wireType == 4 {
-			return fmt.Errorf("proto: QQAccount: wiretype end group for non-group")
-		}
-		if fieldNum <= 0 {
-			return fmt.Errorf("proto: QQAccount: illegal tag %d (wire type %d)", fieldNum, wire)
-		}
-		switch fieldNum {
-		case 1:
-			if wireType != 0 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Nonce", wireType)
-			}
-			m.Nonce = 0
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowClearing
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				m.Nonce |= uint64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-		case 2:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field DepositAccount", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowClearing
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= uint64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthClearing
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex < 0 {
-				return ErrInvalidLengthClearing
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.DepositAccount = string(dAtA[iNdEx:postIndex])
-			iNdEx = postIndex
-		case 3:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field WithdrawAccount", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowClearing
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= uint64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthClearing
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex < 0 {
-				return ErrInvalidLengthClearing
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.WithdrawAccount = string(dAtA[iNdEx:postIndex])
-			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := skipClearing(dAtA[iNdEx:])
@@ -827,40 +539,6 @@ func (m *ClearingAccount) Unmarshal(dAtA []byte) error {
 			m.BtcWithdrawAddress = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		case 4:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field QQAccounts", wireType)
-			}
-			var msglen int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowClearing
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				msglen |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			if msglen < 0 {
-				return ErrInvalidLengthClearing
-			}
-			postIndex := iNdEx + msglen
-			if postIndex < 0 {
-				return ErrInvalidLengthClearing
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.QQAccounts = append(m.QQAccounts, &QQAccount{})
-			if err := m.QQAccounts[len(m.QQAccounts)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
-				return err
-			}
-			iNdEx = postIndex
-		case 5:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field ReserveAccountBalances", wireType)
 			}
