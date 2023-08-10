5db52787d9b71180cd6fcd1dcc18fce28da04703
Hammad Tariq <hammadtariq@yahoo.co.uk>
2023-08-10 20:31:20 +0500

    added reveal commitment check back

--- x/zkos/keeper/msg_server_mint_burn_trading_btc.go
+++ x/zkos/keeper/msg_server_mint_burn_trading_btc.go
@@ -4,17 +4,18 @@ import (
 	"context"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/twilight-project/nyks/x/zkos/types"
 )
 
 func (k msgServer) MintBurnTradingBtc(goCtx context.Context, msg *types.MsgMintBurnTradingBtc) (*types.MsgMintBurnTradingBtcResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// check, err := k.RevealCommitment(msg.QqAccount, msg.EncryptScalar, msg.GetBtcValue())
+	check, err := k.RevealCommitment(msg.QqAccount, msg.EncryptScalar, msg.GetBtcValue())
 
-	// if !check {
-	// 	return nil, sdkerrors.Wrap(types.ErrInvalidCommitment, err.Error())
-	// }
+	if !check {
+		return nil, sdkerrors.Wrap(types.ErrInvalidCommitment, err.Error())
+	}
 
 	// set the mint or burn trading btc
 	errSet := k.SetMintOrBurnTradingBtc(ctx, msg)
